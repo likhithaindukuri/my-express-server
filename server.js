c82684de79ecdb833261bf9468dc3789f1471d83
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-03-15 22:25:33 +0530

    Modified

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express=require("express")
 
 const app=express();
 
-app.get("/")
+app.get("/",function(request,response){
+  console.log("request")
+})
 
 app.listen(3000,function(){
   console.log("Server started at port 3000");

4eafb6026d784e71d526d908719cbb861fb6b8ee
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-03-15 22:33:48 +0530

    Modified

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express=require("express")
 const app=express();
 
 app.get("/",function(request,response){
-  response.send("Hello");
+  response.send("<h1>Hello world!</h1>");
 });
 
 app.listen(3000,function(){

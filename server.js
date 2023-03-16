bad0e8a5a2bbc4f905e0666d496beddff72603d6
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-03-16 22:34:14 +0530

    Modified

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get("/",function(req,res){
   response.send("<h1>Hello world!</h1>");
 });
 
+app.get("/contact",function(req,res){
+  res.send("Contact me at: likhithaindukuri07@gmail.com");
+});
+
+app.get("/about",function(req,ref){
+  res.send("My name is Likhitha");
+});
+
 app.listen(3000,function(){
   console.log("Server started at port 3000");
 });
